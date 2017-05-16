2d45a450d454ab3f35156655acfd98bdee7b55c3
Dave Bokil <bokild@gmail.com>
2017-05-16 12:37:44 -0400

    connection server fix

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,9 @@ var mysql = require("mysql");
 var connection
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-};
-else {
-    var connection = mysql.createConnection({
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+var connection = mysql.createConnection({
         port: 3306,
         host: "localhost",
         user: "root",
@@ -15,6 +14,19 @@ else {
         database: "burgers_db"
     });
 }
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+// };
+// else {
+//     var connection = mysql.createConnection({
+//         port: 3306,
+//         host: "localhost",
+//         user: "root",
+//         password: "",
+//         database: "burgers_db"
+//     });
+// }
 
 // var connection = mysql.createConnection({
 //   port: 3306,
